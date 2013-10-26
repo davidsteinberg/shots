ce805f43a53b7605c0a98d9aefd1b6f0ca9109e3
David Steinberg <davidandrewsteinberg@gmail.com>
2013-10-26 02:03:53 -0400

    moved

--- shots/shot.py
+++ shots/shot.py
@@ -907,3 +907,24 @@ class Shot(object):
 				remove("templates/"+t)
 		
 		return result
+
+#-------------------------
+# Main
+#-------------------------
+
+def main():
+	debugging = False
+	
+	if len(sys.argv) < 2:
+		exit("Usage : " + sys.argv[0] + " <filename> [-d]\n        " + sys.argv[0] + " requires at least a file name as a parameter, with an optional debug flag")
+	elif len(sys.argv) > 2:
+		if sys.argv[2] == "-d":
+			debugging = True
+
+	fileName = sys.argv[1]
+
+	s = Shot(fileName,debug=debugging)
+	print s.generateCode()
+
+if __name__ == "__main__":
+	main()
