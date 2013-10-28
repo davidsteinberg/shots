b5c38cfeb6daf1da6f31c94c1d44b94e8498ae88
David Steinberg <davidandrewsteinberg@gmail.com>
2013-10-28 19:36:28 -0400

    clean directory. about to add src arrays for audio and video

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from shots import Shot
 app = Flask(__name__)
 
 @app.route('/')
-def index(name=None):
+def index():
 	return Shot('index.html').render()
 
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
+
